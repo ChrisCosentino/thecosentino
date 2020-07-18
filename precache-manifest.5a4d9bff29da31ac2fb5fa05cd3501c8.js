f0ab147b11f113db417016a88af9b15d58be51e1
Chris Cosentino <chriscosentino@hotmail.com>
2020-07-18 15:50:51 -0400

    Updates

---
+++ precache-manifest.5a4d9bff29da31ac2fb5fa05cd3501c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d0e329c9695695d831aa758a8d93edd",
+    "url": "/thecosentino/index.html"
+  },
+  {
+    "revision": "576b137101ffa04f4cdc",
+    "url": "/thecosentino/static/css/main.b62d2885.chunk.css"
+  },
+  {
+    "revision": "0f6425b7ff0d178ecb5d",
+    "url": "/thecosentino/static/js/2.b54b55f9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/thecosentino/static/js/2.b54b55f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "576b137101ffa04f4cdc",
+    "url": "/thecosentino/static/js/main.2a32351b.chunk.js"
+  },
+  {
+    "revision": "0e5d86debd707cc798d1",
+    "url": "/thecosentino/static/js/runtime-main.36dbb443.js"
+  }
+]);
